9903ce9a5f3f699e86b478ad0856b9db7c6b5a8d
Martin <6279703+deleonio@users.noreply.github.com>
2023-02-27 21:47:52 +0100

    Update kolibri.test.ts

--- tests/kolibri.test.ts
+++ tests/kolibri.test.ts
@@ -6,7 +6,8 @@ const THEME_CASES = new Set(['bmf', 'bzst', 'desy-v2', 'itzbund', 'mapz', 'th', 
 THEME_CASES.forEach((themeName) => {
 	TEST_CASES.forEach((value, componentName) => {
 		value.forEach((testName) => {
-			createTest(`${themeName}: ${componentName} > ${testName}`, `https://w5u37c.csb.app/#/${componentName}/${testName}?theme=${themeName}`);
+			createTest(`${themeName}: ${componentName} > ${testName}`, `https://cnjn19.csb.app/#/${componentName}/${testName}?theme=${themeName}`); // 1.3
+			// createTest(`${themeName}: ${componentName} > ${testName}`, `https://w5u37c.csb.app/#/${componentName}/${testName}?theme=${themeName}`); // 1.4
 		});
 	});
 });
