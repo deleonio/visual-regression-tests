7c774fd4d1b71f7fcb3d91aa3359c9bb21f7fce4
Martin Oppitz <github@martinoppitz.com>
2023-03-04 07:33:26 +0100

    chore: add ecl themes

--- tests/kolibri.test.ts
+++ tests/kolibri.test.ts
@@ -1,7 +1,7 @@
 import { TEST_CASES } from './src/cases';
 import { createTest } from './src/test';
 
-const THEME_CASES = new Set(['bmf', 'bzst', 'desy-v2', 'itzbund', 'mapz', 'th', 'zoll-v2']);
+const THEME_CASES = new Set(['bmf', 'bzst', 'desy-v2', 'ecl-ec', 'ecl-eu', 'itzbund', 'mapz', 'th', 'zoll-v2']);
 
 THEME_CASES.forEach((themeName) => {
 	TEST_CASES.forEach((value, componentName) => {
