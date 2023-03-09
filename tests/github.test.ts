480f2aa8be5f3809e679bb509e8ae53d9c0dda1e
Martin <6279703+deleonio@users.noreply.github.com>
2023-03-09 14:15:14 +0100

    Update github.test.ts

--- tests/github.test.ts
+++ tests/github.test.ts
@@ -1,16 +1,18 @@
 import { expect, test } from '@playwright/test';
 
-test(`GitHub: Public UI > Follower`, async ({ page }) => {
-	await page.goto(`https://github.com/orgs/public-ui/followers`);
-	await page.waitForSelector('footer');
-	await expect(page).toHaveScreenshot({
-		fullPage: true,
+for(let i = 1; i <= 3; i++) {
+	test(`GitHub: Public UI > Follower - Page ${i}`, async ({ page }) => {
+		await page.goto(`https://github.com/orgs/public-ui/followers?page=${i}`);
+		await page.waitForSelector('footer');
+		await expect(page).toHaveScreenshot({
+			fullPage: true,
+		});
 	});
-});
-test(`GitHub: Public UI > KoliBri > Follower`, async ({ page }) => {
-	await page.goto(`https://github.com/public-ui/kolibri/stargazers`);
-	await page.waitForSelector('footer');
-	await expect(page).toHaveScreenshot({
-		fullPage: true,
+	test(`GitHub: Public UI > KoliBri > Follower - Page ${i}`, async ({ page }) => {
+		await page.goto(`https://github.com/public-ui/kolibri/stargazers?page=${i}`);
+		await page.waitForSelector('footer');
+		await expect(page).toHaveScreenshot({
+			fullPage: true,
+		});
 	});
-});
+]
